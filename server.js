801035803b5a0e00d6d6485b7e7d49ab8d727083
martingerm <martingerm02@gmail.com>
2024-10-24 05:13:16 -0300

    arreglada la conexion

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontendprogiii.vercel.app', // Reemplaza esto con tu URL de frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  credentials: true // Si necesitas permitir cookies
+}));
+
 app.use(express.json());
 app.use('/api/clients', clientRoutes);
 
